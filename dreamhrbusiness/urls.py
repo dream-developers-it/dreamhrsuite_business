571134e872c3f4fca7c28e192b7320fa65866331
dream-developers-it <contact.dream.developers@gmail.com>
2025-01-30 01:36:25 +0600

    url update for robot.txt

--- dreamhrbusiness/urls.py
+++ dreamhrbusiness/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for dreamhrbusiness project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,16 +6,23 @@ from django.views.static import serve
 from django.urls import re_path
 from django.contrib.sitemaps.views import sitemap
 from .sitemap import StaticViewSitemap, DynamicViewSitemap
+from django.http import FileResponse
+import os
 
 sitemaps = {
     'static': StaticViewSitemap,
     'dynamic': DynamicViewSitemap,
 }
 
+def serve_robots_txt(request):
+    robots_file = os.path.join(settings.BASE_DIR, 'robots.txt')
+    return FileResponse(open(robots_file, 'rb'), content_type='text/plain')
+
 urlpatterns = [
     path('dreamhr-business/', admin.site.urls),
     path('', include('dreamhrbusinessweb.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', serve_robots_txt, name='robots_txt'),
     
     # Serve static files in production
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
